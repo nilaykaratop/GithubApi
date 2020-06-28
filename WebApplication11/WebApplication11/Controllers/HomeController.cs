bcbf26a564a5f3989be9cfa9049b3ff55637f467
nilaykaratop <nilay.karatop@gmail.com>
2020-06-28 18:57:45 +0300

    github api

---
+++ WebApplication11/WebApplication11/Controllers/HomeController.cs
@@ -0,0 +1,57 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Diagnostics;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Mvc;
+using WebApplication11.Models;
+using RestSharp;
+using Newtonsoft.Json;
+using WebApplication11.Models;
+namespace WebApplication11.Controllers
+{
+    public class HomeController : Controller
+    {
+        public IActionResult Index()
+        {
+            
+            return View();
+        }
+
+        public IActionResult About()
+        {
+            ViewData["Message"] = "Your application description page.";
+
+            return View();
+        }
+
+        public IActionResult Contact()
+        {
+            ViewData["Message"] = "Your contact page.";
+
+            return View();
+        }
+
+        public IActionResult Privacy()
+        {
+            return View();
+        }
+
+        [ResponseCache(Duration = 0, Location = ResponseCacheLocation.None, NoStore = true)]
+        public IActionResult Error()
+        {
+            return View(new ErrorViewModel { RequestId = Activity.Current?.Id ?? HttpContext.TraceIdentifier });
+        }
+        public IActionResult Repos()
+        {
+            var client = new RestClient("https://api.github.com");
+            var request = new RestRequest("/orgs/dotnet/repos");
+            var content = client.Execute(request).Content;
+            var quertresult = JsonConvert.DeserializeObject<List<Repo>>(content);
+            
+            return View(quertresult);
+           
+        }
+       
+    }
+}
---
+++ WebApplication11/WebApplication11/Models/Repo.cs
@@ -0,0 +1,37 @@
+﻿using Newtonsoft.Json;
+using System;
+using System.Collections.Generic;
+using System.Globalization;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace WebApplication11.Models
+{
+   
+    
+    public class Repo
+    { 
+
+        
+        [JsonProperty("name")]
+        public string Name { get; set; }
+
+        [JsonProperty("description")]
+        public string Description { get; set; }
+
+        [JsonProperty("html_url")]
+        public Uri GitHubHomeUrl { get; set; }
+
+        [JsonProperty("homepage")]
+        public Uri Homepage { get; set; }
+
+        [JsonProperty("watchers")]
+        public int Watchers { get; set; }
+
+        [JsonProperty("pushed_at")]
+        public string JsonDate { get; set; }
+
+       
+
+    }
+}
